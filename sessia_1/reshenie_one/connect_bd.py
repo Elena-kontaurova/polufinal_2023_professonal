f3a6f2e95575f15059b1f0e1a79ba9ea865c8c28
Elena-kontaurova <kontaurova0306@gmail.com>
2025-03-14 23:18:53 +0300

    конец на 14.01.2025

--- sessia_1/reshenie_one/connect_bd.py
+++ sessia_1/reshenie_one/connect_bd.py
@@ -1,6 +1,7 @@
 ''' j'''
 from peewee import Model, MySQLDatabase, IntegerField, ForeignKeyField, \
-                    CharField, DateField, DateTimeField
+                    CharField, DateField, DateTimeField, TextField, \
+                    DecimalField, BooleanField
 
 db = MySQLDatabase('prodi_poluf_2023', user='root', password='lenok',
                    host='localhost', port=3306)
@@ -50,6 +51,102 @@ class Task(DataBase):
     previous_task_id = IntegerField()
 
 
+class Observer(DataBase):
+    ''' k'''
+    task_id = ForeignKeyField(Task, backref='observers')
+    employee_id = IntegerField()
+
+
+class Comment(DataBase):
+    ''' k'''
+    task_id = ForeignKeyField(Task, backref='comments')
+    employee_id = IntegerField()
+    comment_text = TextField()
+    create_date = DateTimeField()
+
+
+class Attachment(DataBase):
+    '''m '''
+    task_id = ForeignKeyField(Task, backref='attachments')
+    file_path = CharField()
+    create_date = DateTimeField()
+
+
+class StatusHistory(DataBase):
+    ''' k'''
+    task_id = ForeignKeyField(Task, backref='status_history')
+    old_status_id = IntegerField()
+    new_status_id = IntegerField()
+    change_date = DateTimeField()
+
+
+class ProjectPortfolio(DataBase):
+    ''' j'''
+    name = CharField()
+    description = TextField()
+    create_date = DateTimeField()
+
+
+class PortfolioProject(DataBase):
+    ''' k'''
+    portfolio = ForeignKeyField(ProjectPortfolio, backref='projects')
+    project = ForeignKeyField(Project, backref='portfolios')
+
+
+class PortfolioCategory(DataBase):
+    ''' k'''
+    parent_category = ForeignKeyField('self', backref='subcategories')
+    name = CharField()
+
+
+class ProjectPortfolioAssociation(DataBase):
+    ''' k'''
+    project = ForeignKeyField(Project, backref='portfolios')
+    portfolio = ForeignKeyField(ProjectPortfolio, backref='project')
+    category = ForeignKeyField(PortfolioCategory, backref='associations')
+
+
+class ProjectAnalytics(DataBase):
+    ''' k'''
+    project = ForeignKeyField(Project, backref='analytics')
+    report_date = DateField()
+    budget = DecimalField()
+    actual_expense = DecimalField()
+    task_completed = IntegerField()
+    task_pending = IntegerField()
+
+
+class User(DataBase):
+    ''' k'''
+    full_name = CharField()
+    organization = CharField()
+
+
+class Email(DataBase):
+    ''' k'''
+    sender = ForeignKeyField(User, backref='send_email')
+    recipient = ForeignKeyField(User, backref='recieved_email')
+    subject = CharField()
+    description = TextField()
+    date_send = DateTimeField()
+    is_replied = BooleanField(default=False)
+
+
+class TaskEmail(DataBase):
+    ''' k'''
+    email = ForeignKeyField(Email, backref='tasks')
+    description = TextField()
+    status = CharField(choices=[('Current', 'Текущая'),
+                                ('Overdue', 'Простроченная')],
+                       default='Current')
+    deadline = DateTimeField()
+    create_date = DateTimeField()
+
+
 db.connect()
-db.create_tables([Project, TaskStatus, Task], safe=True)
+db.create_tables([Project, TaskStatus, Task, Observer, Comment,
+                  Attachment, StatusHistory, ProjectPortfolio,
+                  PortfolioProject, PortfolioCategory,
+                  ProjectPortfolioAssociation, ProjectAnalytics,
+                  User, Email, TaskEmail], safe=True)
 db.close()
